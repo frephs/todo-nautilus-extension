3b2063cc8b1aa29b0507f0bebae8848c5b045f8e
frephs <ffrancescogenovese@gmail.com>
2023-10-13 12:36:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Todo Nautilus File Manager Integration
+
+This Python script provides an extension for the Nautilus file manager, allowing users to mark files as "done" by managing a to-do list file named "todo.md" in the same directory as the file. It utilizes checkboxes to indicate the status of the task associated with each file.
+
+## Usage
+
+1. Clone or download this repository.
+2. Copy the `task_done_extension.py` file to the appropriate Nautilus extensions directory (usually `~/.local/share/nautilus-python/extensions/`).
+3. Restart Nautilus by running `nautilus -q` in the terminal.
+
+## Features
+
+### Task Status Column
+
+A new column named "Done" is added to the file view, indicating whether the associated task is done or not. The column displays a checkmark (✔) for done tasks and an empty checkbox (☐) for pending tasks.
+
+### Toggle Task Status
+
+- Right-click on one or more files in Nautilus.
+- Select "Toggle item" or "Toggle items" from the context menu to mark the selected file(s) as done or undone.
+
+## Implementation Details
+
+The extension utilizes the Nautilus extension API and integrates with the Nautilus file manager to provide the mentioned features. It parses the file paths, checks for the presence of a "todo.md" file, and updates the file icons and the "Done" column accordingly based on the task status.
+
+### Files
+
+- `todo_nautilus_extension.py`: Contains the main extension logic and integrates with Nautilus to provide the functionality.
+  
+### Installation
+- Place the file in the appropriate Nautilus extensions directory (usually `~/.local/share/nautilus-python/extensions/`).
+
+- Restart Nautilus by running `nautilus -q` in the terminal.
+
+
+## Note
+
+This extension assumes the existence of a "todo.md" file in the same directory as the file being examined. The extension considers a task associated with a file as "done" if the file name is found in the "todo.md" file.
+
+For any issues or improvements, feel free to open an issue or submit a pull request.
+
+Enjoy managing your file-rrelated-tasks efficiently with Nautilus!
+
+## Screenshots
+![Nautilus Extension](src/Screenshot.png)
---
+++ todo_nautilus_extension.py
@@ -0,0 +1,121 @@
+import os
+from urllib.parse import unquote, urlparse
+from gi.repository import Nautilus, GObject#, Gdk, Gtk
+import gi
+
+CHECKED_CHAR = u'\u2713'  # Checkmark symbol
+UNCHECKED_CHAR = u'\u2610'  # Empty checkbox symbol
+
+class TaskStatusInfoProvider(GObject.GObject, Nautilus.ColumnProvider, Nautilus.InfoProvider):
+    def get_columns(self):
+        column = Nautilus.Column(
+            name="file_task_done_checked",
+            attribute="file_task_done_checked",
+            label="Done",
+            description="The task associated with the file has been done"
+        )
+
+        return column,
+
+    def update_file_info(self, file):
+        file_path = unquote(urlparse(file.get_uri()).path)
+
+        # Check if todo.md exists in the same directory
+        directory = os.path.dirname(file_path)
+        todo_file_path = os.path.join(directory, "todo.md")
+
+        # # Create a checkbox
+        # checkbox = Gtk.CheckButton()
+        # checkbox.connect("toggled", on_checkbox_toggled)
+
+        # Check if todo.md exists and has the same name as the current file
+        if os.path.exists(todo_file_path):
+            with open(todo_file_path, 'r') as todo_file:
+                todo_file_content = todo_file.read()
+                file_name = os.path.basename(file_path)
+                
+                if file_name in todo_file_content:
+                    # File name found in todo.md, set unchecked checkbox
+                    file.add_emblem('unreadable')  # Replace with appropriate emblem
+                    file.add_string_attribute('file_task_done_checked', UNCHECKED_CHAR)
+                    
+                else:
+                    # File name not found in todo.md, set checked checkbox
+                    file.add_emblem('default')  # Replace with appropriate emblem
+                    file.add_string_attribute('file_task_done_checked', CHECKED_CHAR)
+        else:
+            # Todo.md doesn't exist, set checked checkbox
+            file.add_emblem('checked')  # Replace with appropriate emblem
+            file.add_string_attribute('file_task_done_checked', CHECKED_CHAR)
+
+        # # Get the cell renderer for the "Done" column
+        # cell_renderer = Nautilus.CellRendererPixbuf()
+        # cell_renderer.set_padding(6, 6)
+
+        # # Set the checkbox widget as the pixbuf for the cell renderer
+        # if checkbox.get_active():
+        #     pixbuf = Gtk.IconTheme.get_default().load_icon("checkbox-checked", 16, 0)
+        # else:
+        #     pixbuf = Gtk.IconTheme.get_default().load_icon("checkbox-unchecked", 16, 0)
+
+        # cell_renderer.set_property("pixbuf", pixbuf)
+
+        # # Add the cell renderer to the file info
+        # file.add_info("standard::done_checkbox", cell_renderer)
+
+
+
+
+class ToggleStatusExtension(GObject.GObject, Nautilus.MenuProvider):
+   
+
+    def toggle(self, menu, files):
+
+        # Get the paths for all the files.
+        # Also, strip any protocol headers, if required.
+        # TODO confirm with author:
+        #  windows function doesn't sanitize file names here.
+        #  is this correct? if so this behavior needs to change
+        #  also, this would probably a lot cleaner with pathlib
+
+        for file in files:
+            file_path = unquote(urlparse(file.get_uri()).path)
+            directory = os.path.dirname(file_path)
+            todo_file_path = os.path.join(directory, "todo.md")
+            file_name = os.path.basename(file_path)
+
+            if os.path.exists(todo_file_path):
+                with open(todo_file_path, 'r') as todo_file:
+                    todo_file_content = todo_file.read()
+                # Check if file_name is in todo_file_content
+                if file_name in todo_file_content:
+                    # File name found in todo.md, remove it
+                    todo_file_content = todo_file_content.replace(file_name + '\n', '')
+                    with open(todo_file_path, 'w') as todo_file:
+                        todo_file.write(todo_file_content)
+                else:
+                    # File name not found in todo.md, insert it
+                    with open(todo_file_path, 'a') as todo_file:
+                        todo_file.write(file_name + '\n')
+
+
+    def get_file_items(self, files):
+        # files = args[0] if gi_version_major == 4 else args[1]
+
+        # If there are many items to copy, change the label
+        # to reflect that.
+        if len(files) > 1:
+            item_label = 'Toggle items'
+        else:
+            item_label = 'Toggle item'
+
+        mark_as_done = Nautilus.MenuItem(
+            name='MarkAsDone',
+            label=item_label,
+            tip='Toggle items as done or undone'
+        )
+        mark_as_done.connect('activate', self.toggle, files)
+        #Nautilus.MenuProvider.emit_items_updated_signal();
+        return mark_as_done,
+
+   
